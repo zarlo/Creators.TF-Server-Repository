2d675cf9b1b8701503520a5900aed51803ecebcf
stephanie l <sappho@sappho.io>
2021-08-04 22:59:11 +0000

    Update 2-build.sh

--- scripts/2-build.sh
+++ scripts/2-build.sh
@@ -14,7 +14,7 @@ COMPILED_DIR="plugins"
 # Exclusion lists, use /dir/ for directories and /file_ for file_*.sp
 EXCLUDE_COMPILE="/stac/ /include/ /disabled/ /external/ /economy/ /discord/"
 EXCLUDE_COMPILE="grep -v -e ${EXCLUDE_COMPILE// / -e }"
-EXCLUDE_CLEANUP="/external/"
+EXCLUDE_CLEANUP="/external/ /disabled/"
 EXCLUDE_CLEANUP="grep -v -e ${EXCLUDE_CLEANUP// / -e }"
 
 # Temporary files
